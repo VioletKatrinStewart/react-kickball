7031a3c1fca674b0e9b200ad5aba49cb563d1fc7
Violet Stewart <vkazoo2@gmail.com>
2022-01-03 16:28:22 -0800

    got players list to render on page

--- src/views/Players/Players.js
+++ src/views/Players/Players.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { fetchPlayers } from '../../services/players';
 
 export default function Players() {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchPlayers();
+      setPlayers(data);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div>
+    <div className="players">
       <h1>Players</h1>
+      {players.map((players) => (
+        <Link key={players.id} to={`/players/${players.id}`}>
+          {players.name}
+        </Link>
+      ))}
     </div>
   );
 }
